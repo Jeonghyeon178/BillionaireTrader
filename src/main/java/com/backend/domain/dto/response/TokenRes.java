fcd7598a6c07aaa7576173d30131afffc99628aa
gang1241 <gang1241@gmail.com>
2024-10-06 17:23:34 +0900

    토큰 관리 로직 추가

--- src/main/java/com/backend/domain/dto/response/TokenRes.java
+++ src/main/java/com/backend/domain/dto/response/TokenRes.java
@@ -1,8 +1,15 @@
 package com.backend.domain.dto.response;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record TokenRes(String accessToken, String accessTokenTokenExpired, String TokenType, Integer expiresIn) {
+public record TokenRes(
+	String accessToken,
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	LocalDateTime accessTokenTokenExpired
+) {
 }
--- src/main/java/com/backend/domain/service/ConnectionService.java
+++ src/main/java/com/backend/domain/service/ConnectionService.java
@@ -1,9 +1,9 @@
 package com.backend.domain.service;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -13,27 +13,33 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.backend.domain.dto.mapper.TokenMapper;
 import com.backend.domain.dto.response.TokenRes;
+import com.backend.domain.entity.Token;
+import com.backend.domain.repository.TokenRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ConnectionService {
 
-	private final String URL = "https://openapi.koreainvestment.com:9443/oauth2/tokenP";
 	@Value("${api.app-key}")
 	private String APP_KEY;
 	@Value("${api.app-secret}")
 	private String APP_SECRET;
 	private final ObjectMapper objectMapper;
+	private final TokenMapper tokenMapper;
+	private final TokenRepository tokenRepository;
 
-	public void getAccessToken() throws JsonProcessingException {
+	private void getAccessToken() {
 		RestTemplate restTemplate = new RestTemplate();
 
 		HttpHeaders httpHeaders = new HttpHeaders();
@@ -44,20 +50,69 @@ public class ConnectionService {
 		requestMap.put("appkey", APP_KEY);
 		requestMap.put("appsecret", APP_SECRET);
 
-		String jsonBody = objectMapper.writeValueAsString(requestMap);
+		String jsonBody;
+		try {
+			jsonBody = objectMapper.writeValueAsString(requestMap);
+		} catch (JsonProcessingException e) {
+			// TODO Exception
+			throw new RuntimeException(e);
+		}
 
 		HttpEntity<String> requestMessage = new HttpEntity<>(jsonBody, httpHeaders);
 
+		String URL = "https://openapi.koreainvestment.com:9443/oauth2/tokenP";
 		HttpEntity<String> response = restTemplate.exchange(URL, HttpMethod.POST, requestMessage, String.class);
 		log.info("Response body: {}", response.getBody());
 
 		objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
 
-		TokenRes tokenRes = objectMapper.readValue(response.getBody(), TokenRes.class);
+		TokenRes tokenRes;
+		try {
+			tokenRes = objectMapper.readValue(response.getBody(), TokenRes.class);
+		} catch (JsonProcessingException e) {
+			// TODO Exception
+			throw new RuntimeException(e);
+		}
 
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		Token token = tokenMapper.toToken(tokenRes);
 
-		log.info("Access token obtained: {}", tokenRes.accessToken());
-		log.info("Token expires at: {}", LocalDateTime.parse(tokenRes.accessTokenTokenExpired(), formatter));
+		tokenRepository.save(token);
+
+		log.info("Access token obtained: {}", token.getAccessToken());
+		log.info("Token expires at: {}", token.getExpiration());
+	}
+
+	public void checkValidToken() {
+		Optional<Token> optionalToken = tokenRepository.findTopByOrderByIdDesc();
+
+		LocalDateTime now = LocalDateTime.now();
+
+		optionalToken.ifPresentOrElse(token -> {
+			LocalDateTime expirationTime = token.getExpiration();
+
+			if (expirationTime.isBefore(now)) {
+				log.info("토큰이 만료되었습니다. 새로운 토큰을 요청합니다.");
+				getAccessToken();
+			} else {
+				log.info("토큰은 아직 유효합니다.");
+				log.info(token.getAccessToken());
+			}
+		}, () -> {
+			log.info("저장된 토큰이 없습니다. 새로운 토큰을 요청합니다.");
+			getAccessToken();
+		});
+	}
+
+	public void getAccount() {
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		// httpHeaders.set("authorization", "Bearer " + MyToken);
+		httpHeaders.set("appkey", APP_KEY);
+		httpHeaders.set("appsecrete", APP_SECRET);
+		httpHeaders.set("tr_id", "TTTS3012R");
+
+		Map<String, String> requestMap = new HashMap<>();
+
 	}
 }
